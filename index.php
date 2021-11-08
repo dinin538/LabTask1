0d5cb108f2602ac49ec2d81b8655e651e4cd8844
dinin538 <dnnblh@gmail.com>
2021-11-08 17:30:27 +0800

    edit

--- index.php
+++ index.php
@@ -1,141 +1,64 @@
 <?php
-$item1 = [
-    'Politeknik Ungku Omar' => [
-        'Sarjana Muda Teknologi Kejuteraan Awam Dengan Kepujian'=>[
-            'SPM',
-            'Matrikulasi',
-            'Diploma'
-        ],
-        'Sarjana Muda Teknologi Kejuteraan Pembuatan (Pengurusan Rangkaian Bekalan)'=>[
-            'SPM',
-            'Matrikulasi',
-            'Diploma'
-        ],
-        'Diploma Kejuteraan Awam'=>[
-            'SPM',
-            'Sijil'
-        ],
-        'Diploma Seni Bina'=>[
-            'SPM',
-            'Sijil'
-        ]
+$item = [
+    'Haiwan' => [
+        'kucing',
+        'Tikus',
+        'Ikan',
+        'Monyet'
+
     ],
-    'Politeknik Sultan Haji Ahmad Shah' => [
-        'Diploma Kejuruteraan Awam'=>[
-            'SPM',
-            'Sijil'
-        ],
-        'Diploma Seni Bina'=>[
-            'SPM',
-            'Sijil'
-        ],
-        'Diploma Geomatik'=>[
-            'SPM',
-            'Sijil'
-        ]
-    ]
+    'Bunga' => [
+        'Ros',
+        'Raya',
+        'Orkid',
+        'Magnolia'
+    ],
+    'Benda' => [
+        'Botol',
+        'Kerusi',
+        'Meja',
+        'Jam'
+    ],
 ];
 
-$item2 = [
-    'Politeknik Ungku Omar' => [
-        'Sarjana Muda Teknologi Kejuteraan Awam Dengan Kepujian'=>[
-            'SPM',
-            'STPM'
-        ],
-        
-    ],
-    'Politeknik Sultan Haji Ahmad Shah' => [
-        'Diploma Kejuruteraan Awam'=>[
-            'SPM',
-            'Sijil Kolej Komuniti'
-        ],
-        'Diploma Seni Bina'=>[
-            'SPM',
-            'Sijil Kolej Komuniti'
-        ],
-        'Diploma Geomatik'=>[
-            'SPM',
-            'Sijil Kolej Komuniti'
-        ]
-    ]
-];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <style>
-        *{
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
-        }
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-
-        th{
-            background: orange;
-        }
-    </style>
+    <title>Lab Task 1</title>
+    <link rel="stylesheet" href="style.css">
+    
 </head>
-<body>
-    <h1>Senarai Program di Politeknik</h1>
-    <ul>
-    <?php
-    foreach ($item1 as $poli => $value){
-        echo "<li>$poli";
-        echo "<ul>";
-        foreach ($value as $course =>$test){
-            echo "<li>$course";
-            echo "<ul>";
-            foreach($test as $kelayakan){
-                echo "<li>$kelayakan</li>";
-            }
-            echo '</ul>';
-            echo "</li>";
-        }
-        echo '</ul>';
-        echo "</li>";
-    }
-    ?>
-    </ul>
 
-    <table>
-        <tr style="background: red; color: white">
-            <th><b>Item 1</b></th>
-            <th><b>KELAYAKAN</b></th>
-        </tr>
-    <?php
-    foreach ($item2 as $poli => $value){
-        echo "<th colspan=2>$poli</th>";
-        echo "<tr>";
-        foreach ($value as $course =>$test){
-            echo "<td rowspan=2>$course</td>";
-            
-            foreach($test as $kelayakan){
-                echo "<td>$kelayakan</td>";
-                echo "<tr>";
-                
+<body>
+    <h2>Table 1</h2>
+
+    <table class="center">
+        <?php
+        foreach ($item as $myitem => $value) {
+            echo "<tr> <th>$myitem</th>";
+
+            foreach ($value as $list) {
+                echo "<td>$list</td>";
             }
-            echo '</td>';
-            echo "</tr>";
         }
-        echo '</tr>';
         
-    }
-    ?>
+        echo "</tr>"
+        ?>
+        <!--  <tr>
+            <th>Item 1</th>
+            <td>Value 1</td>
+            <td>Value 2</td>
+            <td>Value 3</td>
+            <td>Value 4</td>
+        </tr> 
+        -->
     </table>
-    <!--<ul>
-        <li>Politeknik Ungku Omar
-            <ul>
-                <li>Sarjana MUda Teknologi Kejuteraan Awam Dengan Kepujian</li>
-                <li></li>
-            </ul>
-        </li>
-    </ul> -->
 </body>
+
 </html>
